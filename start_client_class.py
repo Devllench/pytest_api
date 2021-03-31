fcd3980997c0e05d71cf9f53901298468f958efe
Lenya Sychev <lenya2012@mail.ru>
2021-03-31 03:58:01 +0300

    optimization test

--- start_client_class.py
+++ start_client_class.py
@@ -2,12 +2,12 @@ import requests
 import time
 
 
-def send_request(url_test, valid_data):
+def send_request(url_test):
     # пауза в 1 секунду для соблюдения правил пользования
     # https://operations.osmfoundation.org/policies/nominatim/
     time.sleep(1)
     response = requests.get(url_test)
-    # проверяем есть ли элементы из списка ожидаемых данных в теле ответа
-    search_results = any(
-        valid_string in response.content.decode() for valid_string in valid_data)
-    return search_results
+    if response.status_code == 200:
+        return response.content.decode()
+    elif response.status_code == 200:
+        print("502, a lot of request")
--- test_api.py
+++ test_api.py
@@ -1,27 +1,50 @@
 import pytest
-from data_class import ValidAssertClass
 from start_client_class import send_request
+
+
+def get_test_data(body, left_border, right_border):
+    data = body[body.find(left_border):body.find(right_border)]
+    return data
 
 
 class TestApiClass:
 
     @pytest.mark.parametrize("format_test", ['&format=xml', '&format=json',
                                              '&format=jsonv2', '&format=geojson', '&format=geocodejson'])
-    @pytest.mark.parametrize("query_test", ['q=dybenko+street', 'q=metro+nevskie+prospekt'])
+    @pytest.mark.parametrize("query_test", ['q=metro+nevskie+prospekt'])
     @pytest.mark.parametrize("url_test", ['https://nominatim.openstreetmap.org/search?'])
     def test_search(self, url_test, query_test, format_test):
-        assert send_request(url_test + query_test + format_test, ValidAssertClass.valid_coordinates)
+        body = send_request(url_test + query_test + format_test)
+        if format_test in ['&format=xml']:
+            test_data = get_test_data(body, "lat='", ' display_name')
+            assert test_data == "lat='59.9351318' lon='30.3294301'"
+        elif format_test in ['&format=json', '&format=jsonv2']:
+            test_data = get_test_data(body, '"lat":', ',"display_name"')
+            assert test_data == '"lat":"59.9351318","lon":"30.3294301"'
+        elif format_test in ['&format=geojson', '&format=geocodejson']:
+            test_data = get_test_data(body, 'coordinates":[', ']}}]}')
+            assert test_data == 'coordinates":[30.3294301,59.9351318'
 
     @pytest.mark.parametrize("format_test", ['&format=xml', '&format=json',
                                              '&format=jsonv2', '&format=geojson', '&format=geocodejson'])
-    @pytest.mark.parametrize("coordinates_test", ['lat=59.94080&lon=30.45652', 'lat=60.23105&lon=25.03549'])
+    @pytest.mark.parametrize("coordinates_test", ['lat=59.94080&lon=30.45652'])
     @pytest.mark.parametrize("url_test", ['https://nominatim.openstreetmap.org/reverse?'])
     def test_reverse(self, url_test, coordinates_test, format_test):
-        assert send_request(url_test + coordinates_test + format_test, ValidAssertClass.valid_address)
+        body = send_request(url_test + coordinates_test + format_test)
+        if format_test in ['&format=xml']:
+            test_data = get_test_data(body, "address_rank='30'>", '</result')
+            assert test_data == "address_rank='30'>Metro, 4 литА, проспект Косыгина, округ Пороховые, Санкт-Петербург, Северо-Западный федеральный округ, 195279, Россия"
+        elif format_test in ['&format=json', '&format=jsonv2']:
+            test_data = get_test_data(body, '"display_name":"', '","address"')
+            assert test_data == '"display_name":"Metro, 4 литА, проспект Косыгина, округ Пороховые, Санкт-Петербург, Северо-Западный федеральный округ, 195279, Россия'
+        elif format_test in ['&format=geocodejson']:
+            test_data = get_test_data(body, '"label":', '","name"')
+            assert test_data == '"label":"Metro, 4 литА, проспект Косыгина, округ Пороховые, Санкт-Петербург, Северо-Западный федеральный округ, 195279, Россия'
 
-    @pytest.mark.parametrize("addr_det", ['&addressdetails=1'])
-    @pytest.mark.parametrize("format_test", ['&format=xml'])
-    @pytest.mark.parametrize("coordinates_test", ['q=dybenko+street'])
-    @pytest.mark.parametrize("url_test", ['https://nominatim.openstreetmap.org/search?'])
-    def test_add_details(self, url_test, coordinates_test, format_test, addr_det):
-        assert send_request(url_test + coordinates_test + format_test + addr_det, ValidAssertClass.valid_coordinates)
+
+    # @pytest.mark.parametrize("addr_det", ['&addressdetails=1'])
+    # @pytest.mark.parametrize("format_test", ['&format=xml'])
+    # @pytest.mark.parametrize("coordinates_test", ['q=dybenko+street'])
+    # @pytest.mark.parametrize("url_test", ['https://nominatim.openstreetmap.org/search?'])
+    # def test_add_details(self, url_test, coordinates_test, format_test, addr_det):
+    #     assert send_request(url_test + coordinates_test + format_test + addr_det, ValidAssertClass.valid_coordinates)
